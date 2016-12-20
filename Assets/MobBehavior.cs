5702b2cba51f7c6c3804cc4f653d04884917c1ee
Chris Aasted <caasted@gmail.com>
2016-12-20 11:38:59 -0800

    feat: Implement spawning and random walk behavior

---
+++ Assets/MobBehavior.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobBehavior : MonoBehaviour {
+
+	float randomTaskTimer;
+	float randomTaskWait;
+	Vector3 destination;
+	float maxRange = float.PositiveInfinity;
+
+	// Use this for initialization
+	void Start () {
+		randomTaskTimer = Time.time;
+		randomTaskWait = Random.Range (5, 10);
+		destination = this.transform.position;
+		if (this.GetComponentInParent<MobSpawner> () != null) {
+			maxRange = this.GetComponentInParent<MobSpawner> ().spawnRange;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time - randomTaskTimer > randomTaskWait) {
+			SetNewDestination ();
+		}
+		Vector3 heading = destination - this.transform.position;
+		if (heading.magnitude > 0.1f) {
+			this.transform.position = this.transform.position + (heading * Time.fixedDeltaTime * 0.5f);
+		}
+	}
+
+	void SetNewDestination() {
+		float newX = this.transform.position.x + (Random.value - 0.5f) * 10f;
+		float newZ = this.transform.position.z + (Random.value - 0.5f) * 10f;
+		float newY = Terrain.activeTerrain.SampleHeight (new Vector3 (newX, 0f, newZ)) + this.transform.localScale.y / 2f;
+		destination = new Vector3 (newX, newY, newZ);
+		if (this.transform.localPosition.magnitude > maxRange && this.GetComponentInParent<MobSpawner> () != null) { // Send home
+			destination = this.GetComponentInParent<MobSpawner> ().transform.position;
+		}
+		randomTaskTimer = Time.time;
+		randomTaskWait = Random.Range (5, 10);
+	}
+}
--- Assets/MobSpawner.cs
+++ Assets/MobSpawner.cs
@@ -2,22 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class mobSpawner : MonoBehaviour {
+public class MobSpawner : MonoBehaviour {
 
 	public GameObject mobTemplate;
-	public float spawnRange = 100f;
+	public float spawnRange = 25f;
+	public int spawnCount = 10;
 	public GameObject player;
 
 	// Use this for initialization
 	void Start () {
-		if (mobTemplate == null || player == null)
-			return;
-
-		for (int mobCount = 0; mobCount < 10; mobCount++) {
-			Transform offset = player.transform;
-			offset.position = new Vector3 ((Random.value - 0.5f) * spawnRange, 0f, (Random.value - 0.5f) * spawnRange);
-			offset.position = new Vector3 (offset.position.x, Terrain.activeTerrain.SampleHeight (offset.position), offset.position.z);
-			Instantiate (mobTemplate, offset);
+		if (mobTemplate != null && player != null) {
+			for (int mobCount = 0; mobCount < spawnCount; mobCount++) {
+				var newMob = Instantiate (mobTemplate);
+				newMob.transform.position = this.transform.position;
+				float newX = newMob.transform.position.x + (Random.value - 0.5f) * spawnRange;
+				float newZ = newMob.transform.position.z + (Random.value - 0.5f) * spawnRange;
+				float newY = Terrain.activeTerrain.SampleHeight (new Vector3 (newX, 0f, newZ));
+				newMob.transform.position = new Vector3 (newX, newY, newZ);
+				newMob.transform.SetParent (this.transform);
+				newMob.SetActive (true);
+			}
 		}
 	}
 
