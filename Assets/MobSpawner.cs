543108fc8ce01fbf2e668aa2c6859d6ca13ef908
Chris Aasted <caasted@gmail.com>
2016-12-19 22:32:06 -0800

    feat: Add current files in progress

---
+++ Assets/MobSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mobSpawner : MonoBehaviour {
+
+	public GameObject mobTemplate;
+	public float spawnRange = 100f;
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		if (mobTemplate == null || player == null)
+			return;
+
+		for (int mobCount = 0; mobCount < 10; mobCount++) {
+			Transform offset = player.transform;
+			offset.position = new Vector3 ((Random.value - 0.5f) * spawnRange, 0f, (Random.value - 0.5f) * spawnRange);
+			offset.position = new Vector3 (offset.position.x, Terrain.activeTerrain.SampleHeight (offset.position), offset.position.z);
+			Instantiate (mobTemplate, offset);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
